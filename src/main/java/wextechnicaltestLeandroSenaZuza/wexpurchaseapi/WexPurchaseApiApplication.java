67e868e81af67805edac716e837a370d681bc8c3
08786218654 <leandrosena@lampp-it.com.br>
2023-09-11 22:50:56 -0300

    develop branch

---
+++ src/main/java/wextechnicaltestLeandroSenaZuza/wexpurchaseapi/WexPurchaseApiApplication.java
@@ -0,0 +1,13 @@
+package wextechnicaltestLeandroSenaZuza.wexpurchaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WexPurchaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WexPurchaseApiApplication.class, args);
+	}
+
+}
